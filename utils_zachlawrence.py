c1a349f6ad11798cd5e64c662402eeae097876c8
mzachlawrence <mzl02.lenexa@gmail.com>
2024-09-02 05:40:30 -0500

    Update utils_zachlawrence.py
    Removed beginning comments

--- utils_zachlawrence.py
+++ utils_zachlawrence.py
@@ -1,28 +1,4 @@
-''' ITERATION 1
 
-Module: Stellar Analytics - Reusable Module for My Data Analytics Projects
-
-This module provides a simple, reusable foundation for my analytics projects. 
-When we work hard to write useful code, we want it to be reusable.
-A good byline could be used in every Python analytics project we do.
-
-Process:
-
-We don't write code from top to bottom; instead, we often write it from the outside in.
-Here's what a first draft of my utils_case.py might look like:
-
-1. I start with this docstring at the very beginning.
-   I use it to clarify the purpose of my Python file and organize my thoughts.
-   
-2. I'll declare a global variable for my byline string and just set it to some simple text.
-
-3. I'll declare a main() function for my module. When I run this script, I can use main() to test my byline.
-
-4. I'll add the boilerplate conditional execution code so I only run the main() function when 
-   this script is executed directly (but not when I import it into another file).
-
-I'll test it in an online interpreter to ensure this version runs correctly before continuing.
-'''
 #####################################
 # Import Libraries 
 #####################################
