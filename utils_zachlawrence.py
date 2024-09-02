fea82381594b3c1b408524c0446b03a3da4cd44d
mzachlawrence <mzl02.lenexa@gmail.com>
2024-09-02 06:37:57 -0500

    Update utils_zachlawrence.py
    Iteration 11
    
    small changes to
    
    Offers Clients Additional Skillsets {offers_clients_additional_skillsets}
    Skillsets {skillsets}
    Current Headcount {current_headcount}

--- utils_zachlawrence.py
+++ utils_zachlawrence.py
@@ -98,9 +98,7 @@ Maximum Satisfaction Score:    {max_score}
 Mean Satisfaction Score:       {mean_score:.2f}
 Standard Deviation of Satisfaction Scores: {stdev_score:.2f}
 
-Offers Clients Additional Skillsets {offers_clients_additional_skillsets}
-Skillsets {skillsets}
-Current Headcount {current_headcount}
+C
 
 Corn Market Statistical Examples:
 Corn Daily Close Statistics From:  {closing_price_date_range_start} to {closing_price_date_range_end}
